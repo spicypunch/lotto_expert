423eb50316f040aecaf3139f48ae004e4c0c4659
spicypunch <miniguni66@naver.com>
2024-09-22 02:23:51 +0900

    Feature: isar 초기화 시점 수정 중

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:lotto_expert/riverpod/isar_provider.dart';
 import 'package:lotto_expert/screen/root_screen.dart';
 
 void main() async {
@@ -7,8 +8,30 @@ void main() async {
     ProviderScope(
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: RootScreen(),
+        home: IsarInitializer(),
       ),
     ),
   );
+}
+
+class IsarInitializer extends ConsumerWidget {
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final isarAsyncValue =ref.watch(isarProvider);
+
+    return isarAsyncValue.when(
+      loading: () => const Scaffold(
+        body: Center(
+          child: CircularProgressIndicator(),
+        ),
+      ),
+      error: (error, stackTrace) => Scaffold(
+        body: Center(
+          child: Text('Error: $error'),
+        ),
+      ),
+      data: (isar) => RootScreen(),
+    );
+  }
+
 }
--- lib/riverpod/isar_provider.dart
+++ lib/riverpod/isar_provider.dart
@@ -4,14 +4,21 @@ import 'package:path_provider/path_provider.dart';
 
 import '../model/lotto_model.dart';
 
-final isarProvider = FutureProvider<Isar>((ref) async {
-  if (Isar.instanceNames.isEmpty) {
-    final dir = await getApplicationDocumentsDirectory();
-    return await Isar.open(
-      [LottoModelSchema],
-      inspector: true,
-      directory: dir.path,
-    );
-  }
-  return Future.value(Isar.getInstance());
+final isarProvider = AsyncNotifierProvider<IsarNotifier, Isar?>(() {
+  return IsarNotifier();
 });
+
+class IsarNotifier extends AsyncNotifier<Isar?> {
+  @override
+  Future<Isar?> build() async {
+    if (Isar.instanceNames.isEmpty) {
+      final dir = await getApplicationDocumentsDirectory();
+      return await Isar.open(
+        [LottoModelSchema],
+        inspector: true,
+        directory: dir.path,
+      );
+    }
+    return Isar.getInstance();
+  }
+}
